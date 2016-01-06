47df997eb8972f27b4d21647f1d1b6d6a94de958
rishabh7m <rishabh.makhija07@gmail.com>
2016-01-06 23:04:37 +0530

    Line indicator test spacing

--- ViewPagerIndicatorLib/app/src/main/java/com/rishabh7m/sample/activities/PagerSampleActivity.java
+++ ViewPagerIndicatorLib/app/src/main/java/com/rishabh7m/sample/activities/PagerSampleActivity.java
@@ -26,6 +26,7 @@ public class PagerSampleActivity extends AppCompatActivity {
         circleIndicator.setSpacing(5);
         circleIndicator.setPager(pager);*/
 
+        lineIndicator.setSpacing(10);
         lineIndicator.setPager(pager);
     }
 }
